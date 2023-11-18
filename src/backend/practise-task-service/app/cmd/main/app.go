32be249ff3cf7cf9ff67ec55844d8b4ea7c1e828
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-18 17:22:08 +0300

    Обновил main в соответствии с изменениями

--- src/backend/practise-task-service/app/cmd/main/app.go
+++ src/backend/practise-task-service/app/cmd/main/app.go
@@ -1,26 +1,70 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 	"log"
-	"mod/internal/handler"
-	"mod/internal/storage"
-	"mod/internal/storage/postgres"
-	"mod/internal/usecase"
+	"net/http"
+	"os"
+	"os/signal"
+	"practise-task-service/pkg/handler"
+	"practise-task-service/pkg/storage"
+	"practise-task-service/pkg/storage/postgres"
+	"practise-task-service/pkg/usecase"
+	"syscall"
 )
 
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env не найден")
+	}
+}
+
 func main() {
+	err := initConfig()
+	if err != nil {
+		log.Fatalf("Ошибка в чтении cfg - %s\n", err)
+	}
+
 	db, err := postgres.New(postgres.Config{
-		Host:     "",
-		Port:     "",
-		Username: "",
-		Password: "",
-		DBName:   "",
-		SSLMode:  "",
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   "postgres",
+		SSLMode:  viper.GetString("db.sslmode"),
 	})
 	if err != nil {
 		log.Fatalf("Ошибка в инициалзиации бд - %s", err)
 	}
-	repository := storage.New(db)
-	service := usecase.New(repository)
-	handler := handler.New(service)
+	repo := storage.New(db)
+	service := usecase.New(repo)
+	handlers := handler.New(service)
+
+	srv := &http.Server{
+		Addr:         viper.GetString("http_server.address"),
+		Handler:      handlers.Init(),
+		ReadTimeout:  viper.GetDuration("http_server.timeout"),
+		WriteTimeout: viper.GetDuration("http_server.timeout"),
+		IdleTimeout:  viper.GetDuration("http_server.idle_timeout"),
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	log.Println("Запуск сервера")
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("ошибка в запускее сервера - %s\n", err)
+		}
+	}()
+	<-done
+	log.Println("Остановка сервера")
+
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("dev")
+
+	return viper.ReadInConfig()
 }
--- src/backend/practise-task-service/app/go.mod
+++ src/backend/practise-task-service/app/go.mod
@@ -1,7 +1,45 @@
-module mod
+module practise-task-service
 
 go 1.21.1
 
 require github.com/jmoiron/sqlx v1.3.5
 
-require github.com/go-chi/chi/v5 v5.0.10 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/jackc/pgx/v4 v4.18.1
+)
+
+require (
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/essentialkaos/translit/v2 v2.1.1 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-chi/render v1.0.3 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/sagikazarmark/locafero v0.3.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.10.0 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.17.0 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
