c15dcb747cd783de6a041ff92e8a729bff3639e0
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-19 23:17:45 +0300

    вспомогательная функция для оборачивания ошибочных ответов

---
+++ src/backend/practise-task-service/app/pkg/handler/response-wrapper.go
@@ -0,0 +1,15 @@
+package handler
+
+import (
+	"github.com/go-chi/render"
+	"net/http"
+)
+
+type ErrResp struct {
+	Message string `json:"message"`
+}
+
+func NewErrorResponse(w http.ResponseWriter, r *http.Request, code int, message string) {
+	w.WriteHeader(code)
+	render.JSON(w, r, ErrResp{Message: message})
+}
