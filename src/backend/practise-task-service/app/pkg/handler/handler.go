dd84a5a627aaab71e48ee47c50440ad8a866fe77
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-19 23:16:18 +0300

    зарегестрировал middleware для проверки формата входящего файла

--- src/backend/practise-task-service/app/pkg/handler/handler.go
+++ src/backend/practise-task-service/app/pkg/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"github.com/go-chi/chi/v5"
+	"practise-task-service/pkg/handler/mw"
 	"practise-task-service/pkg/usecase"
 )
 
@@ -22,7 +23,10 @@ func (h *Handler) Init() *chi.Mux {
 		r.Get("/practice-tasks", h.GetAllPracticeTask())
 		r.Get("/practice-tasks/{id}", h.GetPractice())
 		r.Get("/practice-tasks/search?title={t}item={i}", h.SearchPractice())
-		r.Post("/practice-tasks", h.UploadPractice())
+		r.Group(func(r chi.Router) {
+			r.Use(mw.ExtensionValidator)
+			r.Post("/practice-tasks", h.UploadPractice())
+		})
 		r.Delete("/practice-tasks/{id}", h.DeletePractice())
 	})
 
