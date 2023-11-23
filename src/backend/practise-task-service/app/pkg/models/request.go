0c43b1403f4c954425b945274ee7a3f568acff83
Santixtx <danil.shirin.04@mail.ru>
2023-11-23 14:34:29 +0300

    Добавил в информацию о добавлении автора в модели

--- src/backend/practise-task-service/app/pkg/models/request.go
+++ src/backend/practise-task-service/app/pkg/models/request.go
@@ -4,6 +4,7 @@ import "mime/multipart"
 
 type UploadPracticeRequest struct {
 	Title           string   `json:"title"`
+	Author          string   `json:"author"`
 	Theme           string   `json:"theme"`
 	AcademicSubject string   `json:"academicSubject"`
 	AccessGroup     []string `json:"accessGroup"`
