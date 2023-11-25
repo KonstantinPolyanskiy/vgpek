17aac5b2b7bdc38d38c7611cb4b2ec5ef16822d2
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-25 19:55:53 +0300

    Небольшое исправление в хендлерах

--- src/backend/practise-task-service/app/pkg/handler/handler.go
+++ src/backend/practise-task-service/app/pkg/handler/handler.go
@@ -23,10 +23,7 @@ func (h *Handler) Init() *chi.Mux {
 		r.Get("/practice-tasks", h.GetAllPracticeTask())
 		r.Get("/practice-tasks/{id}", h.GetPractice())
 		r.Get("/practice-tasks/search?title={t}item={i}", h.SearchPractice())
-		r.Group(func(r chi.Router) {
-			r.Use(mw.ExtensionValidator)
-			r.Post("/practice-tasks", h.UploadPractice())
-		})
+		r.With(mw.ExtensionValidator).Post("/practice-tasks", h.UploadPractice())
 		r.Delete("/practice-tasks/{id}", h.DeletePractice())
 	})
 
