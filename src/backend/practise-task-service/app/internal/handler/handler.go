d22ebc147b7d8e1631ad4a8c9639b830d3e640c1
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-26 00:54:59 +0300

    Прокинул логгер в слой обработчиков и применил его

--- src/backend/practise-task-service/app/internal/handler/handler.go
+++ src/backend/practise-task-service/app/internal/handler/handler.go
@@ -2,28 +2,33 @@ package handler
 
 import (
 	"github.com/go-chi/chi/v5"
+	"log/slog"
 	"practise-task-service/internal/handler/mw"
 	"practise-task-service/internal/usecase"
 )
 
 type Handler struct {
 	service *usecase.Service
+	logger  *slog.Logger
 }
 
-func New(service *usecase.Service) *Handler {
+func New(service *usecase.Service, logger *slog.Logger) *Handler {
 	return &Handler{
 		service: service,
+		logger:  logger,
 	}
 }
 
 func (h *Handler) Init() *chi.Mux {
 	r := chi.NewRouter()
 
+	r.Use(mw.Logger(h.logger))
+
 	r.Route("/api", func(r chi.Router) {
 		r.Get("/practice-tasks", h.GetAllPracticeTask())
 		r.Get("/practice-tasks/{id}", h.GetPractice())
 		r.Get("/practice-tasks/search", h.SearchPractice())
-		r.With(mw.ExtensionValidator).Post("/practice-tasks", h.UploadPractice())
+		r.With(mw.ExtensionValidator(h.logger)).Post("/practice-tasks", h.UploadPractice())
 		r.Delete("/practice-tasks/{id}", h.DeletePractice())
 	})
 
