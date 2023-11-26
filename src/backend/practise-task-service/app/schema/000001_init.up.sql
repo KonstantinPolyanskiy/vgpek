d49694306475d06f335cae200869fb4a09cc4e95
KonstantinPolyanskiy <polyanskiy.main@list.ru>
2023-11-26 23:29:00 +0300

    Добавил индексы для поиска

--- src/backend/practise-task-service/app/schema/000001_init.up.sql
+++ src/backend/practise-task-service/app/schema/000001_init.up.sql
@@ -11,11 +11,13 @@ CREATE TABLE access_groups (
     Group_Id SERIAL PRIMARY KEY,
     Group_Name varchar
 );
-
-CREATE TABLE PracticeAccess (
+CREATE TABLE practice_access (
                             Practice_Id INT,
                             Group_Id INT,
                             PRIMARY KEY (Practice_Id, Group_Id),
                             FOREIGN KEY (Practice_Id) REFERENCES practice_info(Id),
                             FOREIGN KEY (Group_Id) REFERENCES access_groups(Group_Id)
 );
+CREATE INDEX idx_title_tsvector ON practice_info USING gin(to_tsvector('russian', title));
+CREATE INDEX idx_subject_tsvector ON practice_info USING gin(to_tsvector('russian', academic_subject))
+
